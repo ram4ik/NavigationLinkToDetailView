2699771d938acef85b99a53f699d26ebfed0dbef
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-11-29 12:41:43 +0200

    init

--- NavigationLinkToDetailView/ContentView.swift
+++ NavigationLinkToDetailView/ContentView.swift
@@ -8,9 +8,54 @@
 
 import SwiftUI
 
+struct InfoView: Identifiable {
+    var id: Int
+    let name, image: String
+}
+
 struct ContentView: View {
+    var infos: [InfoView] = [
+        .init(id: 0, name: "Estonia", image: "estonia"),
+        .init(id: 1, name: "Latvia", image: "latvia"),
+        .init(id: 2, name: "Lithuania", image: "lithuania"),
+        .init(id: 3, name: "Finland", image: "finland"),
+    ]
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            List {
+                ForEach(infos, id: \.id) { info in
+                    NavigationLink(destination: VStack(alignment: .leading) {
+                        Image(info.image)
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(12)
+                        HStack {
+                            Text(info.name)
+                                .font(.headline)
+                            Spacer()
+                            Image(systemName: "heart.fill")
+                            .foregroundColor(.red)
+                        }
+                    }.padding()) {
+                        
+                        VStack(alignment: .leading) {
+                            Image(info.image)
+                                .resizable()
+                                .scaledToFit()
+                                .cornerRadius(12)
+                            HStack {
+                                Text(info.name)
+                                    .font(.headline)
+                                Spacer()
+                                Image(systemName: "heart")
+                                    .foregroundColor(.red)
+                            }
+                        }
+                    }
+                }
+            }.navigationBarTitle("Lands")
+        }
     }
 }
 
